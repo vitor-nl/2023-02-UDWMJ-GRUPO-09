1eb2fd3971dd7525badb77e74ffd4799cfc61f81
samuereali <realinski@gmail.com>
2023-10-10 16:39:27 -0300

    criação classes SocialNetwork - ClientSocialnetwor

---
+++ ClientSocialnetwork.py
@@ -0,0 +1,7 @@
+from SocialNetwork import SocialNetwork
+from Cliente import Cliente
+
+class ClientSocialnetwork:
+    def __init__(self) -> None:
+        self.__client = Cliente()
+        self.__socialnetwork = SocialNetwork()
